c4155765098e486692ff166a0a99b6f43113f7e0
2002MONU <monusaini1663197@gmail.com>
2025-03-08 15:30:00 +0530

    fit

---
+++ sorting.php
@@ -0,0 +1,16 @@
+<?php 
+ 
+//    echo "Monu Kumar";
+
+$a = array('Monu','Sonu','Sunil','Nitin');
+
+//  sort($a); // ascending array in indexes array
+ rsort($a); // descending array in indexes array
+
+echo "<pre>";
+print_r($a);
+echo "</pre>";
+
+
+
+?>
